d0aef912752775326e2c0699f0055a649394839d
idefkicabti <127599773+idefkicabti@users.noreply.github.com>
2023-11-22 18:27:56 +0800

    Update ChangeMessageButtonListener.java

--- Activity_for_java_2/src/act/ChangeMessageButtonListener.java
+++ Activity_for_java_2/src/act/ChangeMessageButtonListener.java
@@ -6,7 +6,7 @@ import javax.swing.JLabel;
 
 //This class implements the ActionListener interface, which means it can respond to events (like button clicks)
 public class ChangeMessageButtonListener implements ActionListener{
-	// A reference to a JLabel that this listener will modify
+    // A reference to a JLabel that this listener will modify
 	private JLabel label;
     // The constructor takes a JLabel as a parameter and stores it in the 'label' field
     public ChangeMessageButtonListener(JLabel label) {
