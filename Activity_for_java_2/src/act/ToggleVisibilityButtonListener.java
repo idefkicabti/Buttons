d8216356d0d10afad50808e85df8bf6b3a135c60
idefkicabti <127599773+idefkicabti@users.noreply.github.com>
2023-11-22 18:12:41 +0800

    Update ToggleVisibilityButtonListener.java

--- Activity_for_java_2/src/act/ToggleVisibilityButtonListener.java
+++ Activity_for_java_2/src/act/ToggleVisibilityButtonListener.java
@@ -6,22 +6,32 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
+// This class implements ActionListener to respond to button clicks
 public class ToggleVisibilityButtonListener implements ActionListener{
-	private JLabel label;
-	private JButton button;
+    // The JLabel and JButton that this listener will modify
+    private JLabel label;
+    private JButton button;
 
+    // Constructor for the ToggleVisibilityButtonListener class
     public ToggleVisibilityButtonListener(JLabel label, JButton button) {
+        // Initialize the label and button
         this.label = label;
         this.button = button;
     }
 
+    // This method is called whenever the button is clicked
     @Override
     public void actionPerformed(ActionEvent e) {
-    	if (label.isVisible()) {
+        // If the label is currently visible
+        if (label.isVisible()) {
+            // Hide the label
             label.setVisible(false);
+            // Change the button text to "Show Message"
             button.setText("Show Message");
         } else {
+            // Otherwise, show the label
             label.setVisible(true);
+            // And change the button text to "Hide Message"
             button.setText("Hide Message");
         }
     }
